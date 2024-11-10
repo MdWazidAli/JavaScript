1523c60ee5d4230b08c141d821dcd61e08bdd04e
MdWazidAli <abdullah856164@gmail.com>
2024-11-10 11:37:33 -0500

    This File JavaScript

---
+++ Test.js
@@ -0,0 +1,17 @@
+console.log("heloo jii");
+console.log(Math.round(2.3));
+
+// Concatenatio In String
+
+console.log('Harry' + 'Potter');
+
+console.log(typeof 2);
+
+console.log('$' + (20.95 + 7.99));
+
+console.log('$' + (2095 + 799) / 100);
+
+console.log('items(' + (1 + 1) + '): $' + (2095 + 799) / 100);
+
+// alert('items(' + (1 + 1) + '): $' + (2095 + 799) / 100);
+
